4ff90ef10fb8f11cc49c54e81bf14b91a4a0be9f
Serkan Ozsoz <serkanozsoz@hotmail.com>
2022-03-23 22:31:49 +0300

    Metot Tanımlama

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -14,22 +14,22 @@ namespace Methods // Note: actual namespace depends on the project name.
             int a = 2;
             int b = 3;
 
-            Console.WriteLine(a+b);
+            Console.WriteLine(a + b);
 
-            int sonuc = Topla(a,b);
+            int sonuc = Topla(a, b);
             System.Console.WriteLine(sonuc);
 
             Metotlar ornek = new Metotlar();
             ornek.EkranaYazdir(Convert.ToString(sonuc));
-            
-            int sonuc2 = ornek.ArttirveTopla(ref a,ref b);
+
+            int sonuc2 = ornek.ArttirveTopla(ref a, ref b);
             System.Console.WriteLine(Convert.ToString(sonuc2));
-            System.Console.WriteLine(Convert.ToString(a+b));
+            System.Console.WriteLine(Convert.ToString(a + b));
 
         }
         static int Topla(int deger1, int deger2)
         {
-            return (deger1+deger2);
+            return (deger1 + deger2);
         }
 
     }
@@ -43,12 +43,9 @@ namespace Methods // Note: actual namespace depends on the project name.
 
         public int ArttirveTopla(ref int deger1, ref int deger2)
         {
-            deger1+=1;
-            deger2+=1;
-            return deger1+deger2;
+            deger1 += 1;
+            deger2 += 1;
+            return deger1 + deger2;
         }
     }
-
-        
-    
 }
