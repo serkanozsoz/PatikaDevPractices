87902590ff6615959c850857f215469c814b017f
Serkan Ozsoz <serkanozsoz@hotmail.com>
2022-04-10 16:10:17 +0300

    Değişkenler ve Veri Tipleri

--- variables/Program.cs
+++ variables/Program.cs
@@ -6,30 +6,30 @@ namespace Variables
     {
         static void Main(string[] args)
         { 
-            byte b = 5;             //1 byte
-            sbyte c = 5;            //1 byte
+            // byte b = 5;             //1 byte
+            // sbyte c = 5;            //1 byte
 
-            short s = 5;            //2 byte
-            ushort us = 5;          //2 byte
+            // short s = 5;            //2 byte
+            // ushort us = 5;          //2 byte
 
-            Int16 i16 = 2;          //2 byte
-            int i = 2;              //4 byte
-            Int32 i32 = 2;          //4 byte
-            Int64 i64 = 2;          //8 byte
+            // Int16 i16 = 2;          //2 byte
+            // int i = 2;              //4 byte
+            // Int32 i32 = 2;          //4 byte
+            // Int64 i64 = 2;          //8 byte
 
-            uint ui = 2;            //4 byte
-            long l = 4;             //8 byte
-            ulong ul = 4;           //8 byte
+            // uint ui = 2;            //4 byte
+            // long l = 4;             //8 byte
+            // ulong ul = 4;           //8 byte
 
-            float f = 5;            //4 byte
-            double d = 5;           //8 byte
-            decimal de = 5;         //16 byte
+            // float f = 5;            //4 byte
+            // double d = 5;           //8 byte
+            // decimal de = 5;         //16 byte
 
-            char ch = '2';          //2 byte
-            string str = "Anıl";   //sınırsız
+            // char ch = '2';          //2 byte
+            // string str = "Anıl";   //sınırsız
 
-            bool b1 = true;
-            bool b2 = false;
+            // bool b1 = true;
+            // bool b2 = false;
 
             DateTime dt = DateTime.Now;
 
@@ -48,7 +48,7 @@ namespace Variables
             int integer2 = 3;
             int integer3 = integer1 * integer2;
 
-            bool bool1 = 10 < 2;
+            // bool bool1 = 10 < 2;
 
             string str20 = "20";
             int int20 = 20;
